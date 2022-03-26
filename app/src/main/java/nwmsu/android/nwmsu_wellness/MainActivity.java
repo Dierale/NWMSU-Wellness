8029e4e8d24cce376d7ee6f5d8bccb30febe75c2
Dierale <89657232+Dierale@users.noreply.github.com>
2022-03-25 22:44:26 -0500

    Update MainActivity.java
    Added default randomized daily quote via hard coded string array.  Represents data we would pull from a small web server later if possible.

--- app/src/main/java/nwmsu/android/nwmsu_wellness/MainActivity.java
+++ app/src/main/java/nwmsu/android/nwmsu_wellness/MainActivity.java
@@ -3,6 +3,9 @@ package nwmsu.android.nwmsu_wellness;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView dailyQuoteTV = findViewById( R.id.dailyQuoteTV);
+        dailyQuoteTV.setText( getDailyQuote());
+    }
+
+    public String getDailyQuote() {
+        // Should be keeping this inside a string resource
+        String[] quotes = {
+                "A Bearcat a day keeps the angry geese away",
+                "Squirrels are a Bearcat's best friend",
+                "When in doubt, wear green out",
+                "Motivational quotes aren't necessary when you're awesome",
+                "There's no motivation like impending doom"
+        };
+        final int maxSize = quotes.length;
+        // ((Max - Min) + 1) + Min // Our min is 0 right now
+        final int random = new Random().nextInt(maxSize+ 1);
+        return "Daily Quote\n"+quotes[random];
     }
 }
