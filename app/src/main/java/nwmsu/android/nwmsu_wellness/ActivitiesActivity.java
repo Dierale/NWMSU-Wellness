8ad54430db1a399f0ad9afb49a2d3ac9dc6c1de2
Reason Adhikari <72090971+adhikariReason@users.noreply.github.com>
2022-03-25 19:08:23 -0500

    Embedded webView in activity_activities.xml

--- app/src/main/java/nwmsu/android/nwmsu_wellness/ActivitiesActivity.java
+++ app/src/main/java/nwmsu/android/nwmsu_wellness/ActivitiesActivity.java
@@ -3,12 +3,23 @@ package nwmsu.android.nwmsu_wellness;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class ActivitiesActivity extends AppCompatActivity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activities);
+
+
+        webView =findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.google.com");
+
     }
 }
