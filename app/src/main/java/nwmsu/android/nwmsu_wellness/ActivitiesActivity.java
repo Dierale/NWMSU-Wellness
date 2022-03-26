7ee8161814a6aecbe33743ded341f055fb207186
Reason Adhikari <72090971+adhikariReason@users.noreply.github.com>
2022-03-25 20:28:29 -0500

    Linked "Add to Calendar" button to add event activity

--- app/src/main/java/nwmsu/android/nwmsu_wellness/ActivitiesActivity.java
+++ app/src/main/java/nwmsu/android/nwmsu_wellness/ActivitiesActivity.java
@@ -2,10 +2,13 @@ package nwmsu.android.nwmsu_wellness;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 
 public class ActivitiesActivity extends AppCompatActivity {
 
@@ -19,7 +22,31 @@ public class ActivitiesActivity extends AppCompatActivity {
 
         webView =findViewById(R.id.webView);
         webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("https://www.google.com");
+        webView.loadUrl("http://calendar.nwmissouri.edu/");
 
+        Button addCalanderBTN = findViewById(R.id.addCalanderBTN);
+        addCalanderBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent eventsIntent = new Intent( getApplicationContext(), EventActivity.class);
+                    startActivity(eventsIntent);
+                } catch (Exception e) {
+                    // Error handling
+                    Log.d("click", "Error on: click add button ");
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }
+        else{
+            super.onBackPressed();
+        }
     }
 }
