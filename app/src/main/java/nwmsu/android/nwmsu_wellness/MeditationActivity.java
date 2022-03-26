46603a53512bc0e2ccea251e1702aeef9662e2fd
Daniel1012345 <72279708+Daniel1012345@users.noreply.github.com>
2022-03-25 23:43:34 -0500

    Meditation activity

--- app/src/main/java/nwmsu/android/nwmsu_wellness/MeditationActivity.java
+++ app/src/main/java/nwmsu/android/nwmsu_wellness/MeditationActivity.java
@@ -2,7 +2,13 @@ package nwmsu.android.nwmsu_wellness;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MeditationActivity extends AppCompatActivity {
 
@@ -10,5 +16,25 @@ public class MeditationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meditation);
+        ((ImageButton) findViewById(R.id.phoneBTN)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phno = "6605621348";
+                Intent i = new Intent(Intent.ACTION_DIAL, Uri.parse(phno));
+                startActivity(i);
+
+            }
+        });
+        ImageButton bt = (ImageButton) findViewById(R.id.musicBTN);
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://www.youtube.com/watch?v=1ZYbU82GVz4");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
     }
 }
