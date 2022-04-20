8763b0de4be260dc6447efee4a414c15e491bcb0
Daniel1012345 <72279708+Daniel1012345@users.noreply.github.com>
2022-04-19 23:43:46 -0500

    MeditationActivity

--- app/src/main/java/nwmsu/android/nwmsu_wellness/MeditationActivity.java
+++ app/src/main/java/nwmsu/android/nwmsu_wellness/MeditationActivity.java
@@ -3,12 +3,9 @@ package nwmsu.android.nwmsu_wellness;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -21,7 +18,7 @@ public class MeditationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meditation);
 
-        TextView motivQuoteTV = findViewById( R.id.QuoteTV);
+        TextView motivQuoteTV = findViewById( R.id.quoteTV);
         motivQuoteTV.setText( getMotivQuote());
 
         ImageButton phonebt = (ImageButton) findViewById(R.id.phoneBTN);
